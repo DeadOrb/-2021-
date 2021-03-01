77cd696160bcd485151c6e2a3d195af50c9a9c23
mikleleb <47142970+mikleleb@users.noreply.github.com>
2021-03-02 01:52:40 +0300

    Add files via upload
    Add test file for run ariphmetic encoder for testing system usability

---
+++ ariphm_encode.py
@@ -0,0 +1,40 @@
+def get_probs(alphabet, text):
+    probs = [0] * len(alphabet)
+    for i, a in enumerate(alphabet):
+        probs[i] = text.count(a) / len(text)
+    return probs
+def define_segments(alphabet, probs):
+    segments = dict()
+    l = 0
+    for i, c in enumerate(alphabet):
+        left = l
+        right = l + probs[i]
+        segments[c] = (left, right) # left border, right border
+        l = right
+    return segments
+def ariphmetic_encode(alphabet, probs, text): 
+    segments = define_segments(alphabet, probs)
+    # print(segments)
+    left = 0
+    right = 1
+    for i, letter in enumerate(text):
+        left, right  = left + (right - left) * segments[letter][0], left + (right - left) * segments[letter][1]
+        # print(left, right)
+    return (left + right) / 2
+def define_segments2(alphabet, probs):
+    segments = []
+    l = 0.0
+    for i, c in enumerate(alphabet):
+        left = l
+        right = l + probs[i]
+        segments[c] = (left, right) # left border, right border, alphabet[i]
+        l = right
+        return segments
+def ariphmetic_decode(alphabet, probs, code, text_len, segments):
+    segments = define_segment2(alphabet, probs)
+    res = []
+    for i in range(text_len):
+        for segm in segments:
+            left, right = segments
+    
+    return ''.join(res)
